6715ed455fc310de925f85ec6c705d8e8e2066e1
Santiago <shinca12@eafit.edu.co>
2018-11-29 17:09:04 -0500

    yadda yadda

---
+++ notebooks/lectures/Utils/lect1.py
@@ -0,0 +1,21 @@
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_1D_dataset_samples(n, d1, d2, n_datasets=10, dot_alpha=.5, line_alpha=.5, figsize=(20,5)):
+    plt.figure(figsize=figsize)
+    for i in range(n_datasets):
+
+        m1 = d1.rvs(n)
+        m2 = d2.rvs(n)
+        X = np.append(m1, m2).reshape(-1,1)
+        y = np.r_[[0]*len(m1)+[1]*len(m2)]
+        estimator = DecisionTreeClassifier(max_depth=1)
+        estimator.fit(X,y)
+        Xr = np.linspace(5, 30, 100).reshape(-1,1)
+        yr = estimator.predict(Xr)
+        plt.plot(Xr[yr==0], [i]*np.sum(yr==0), color="red", alpha=line_alpha, lw=4)
+        plt.plot(Xr[yr==1], [i]*np.sum(yr==1), color="blue", alpha=line_alpha, lw=4)
+        plt.scatter(m1, [i+.1]*len(m1), color="red", alpha=dot_alpha, s=100)
+        plt.scatter(m2, [i+.1]*len(m2), color="blue", alpha=dot_alpha, s=100)
+    plt.axis("off")
